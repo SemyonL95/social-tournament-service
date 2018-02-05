393a7b4febfda199ecf11d08357315b114994b51
Semyon Loboichenko <semyonl1995@gmail.com>
2018-02-05 22:45:37 +0200

    Base docker setup

--- src/main.go
+++ src/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"log"
 )
 
 func main() {
-	fmt.Println("hello world")
+
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal("ERROR DURING SERVING")
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
