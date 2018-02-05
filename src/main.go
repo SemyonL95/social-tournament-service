a8cd683d0aff5ba9ed26cd461578c469634cd518
Semyon Loboichenko <semyonl1995@gmail.com>
2018-02-05 23:32:45 +0200

    Docker env setup with base database test

--- src/main.go
+++ src/main.go
@@ -22,11 +22,20 @@ func main() {
 		"5432",
 	)
 
-	_, err := sql.Open("postgres", connInfo)
+	var err error
+	db, err = sql.Open("postgres", connInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	_, err = db.Exec(
+		`create table if not exists mydata (
+			id serial primary key,
+			val integer not null
+		)`)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	http.HandleFunc("/", serveIndex)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -37,4 +46,28 @@ func main() {
 func serveIndex(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprintln(w, "Hello, World!\n")
+
+	_, err := db.Exec("insert into mydata(val) values(1)")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := db.Query("select id from mydata")
+	defer rows.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var id int
+
+		err = rows.Scan(&id)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Fprintf(w, "ID: %d\n", id)
+	}
 }
