88842fe9cd231658fce35bd1f39dc0ae7ab0ca30
Semyon Loboichenko <semyonl1995@gmail.com>
2018-02-05 22:46:26 +0200

    Base docker setup

--- src/main.go
+++ src/main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
+	_ "github.com/lib/pq"
+)
+
+var (
+	db *sql.DB
 )
 
 func main() {
+	connInfo := fmt.Sprintf(
+		"user=%s dbname=%s password=%s host=%s port=%s sslmode=disable",
+		"postgres",
+		"postgres",
+		"mypass",
+		"db",
+		"5432",
+	)
 
-	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":8080", nil)
-
+	_, err := sql.Open("postgres", connInfo)
 	if err != nil {
-		log.Fatal("ERROR DURING SERVING")
+		log.Fatal(err)
+	}
+
+
+	http.HandleFunc("/", serveIndex)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "Hi there, I love %s!", r.URL.Path[1:])
+func serveIndex(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintln(w, "Hello, World!\n")
 }
